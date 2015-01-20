3700f7c8046becb3af5d3b295a3e011311f8c95b
Martin Lantzsch <martin@linux-doku.de>
2015-01-20 23:34:38 +0100

    formated parser

--- src/parser.js
+++ src/parser.js
@@ -1,60 +1,60 @@
 module.exports = {
-	parse: function(expression) {
-		var pushWord = function(words, word, inSub) {
-			if(word !== '') {
-				if(inSub) {
-					if(words[words.length - 1] instanceof Array) {
-						words[words.length - 1].push(word);
-					} else {
-						words.push([word]);
-					}
-				} else {
-					words.push(word);
-				}
-			}
-		}
+    parse: function(expression) {
+        var pushWord = function(words, word, inSub) {
+            if (word !== '') {
+                if (inSub) {
+                    if (words[words.length - 1] instanceof Array) {
+                        words[words.length - 1].push(word);
+                    } else {
+                        words.push([word]);
+                    }
+                } else {
+                    words.push(word);
+                }
+            }
+        }
 
-		var words = [],
+        var words = [],
             word = '';
 
-		var inQuotes = false,
-			inSub = false;
+        var inQuotes = false,
+            inSub = false;
 
-		for(var i = 0; i < expression.length; i++) {
-			var character = expression[i];
+        for (var i = 0; i < expression.length; i++) {
+            var character = expression[i];
 
-			if(!inQuotes) {
-				if(character === ' ') {
-					pushWord(words, word, inSub);
-					word = '';
-					continue;
-				}
+            if (!inQuotes) {
+                if (character === ' ') {
+                    pushWord(words, word, inSub);
+                    word = '';
+                    continue;
+                }
 
-				if(character === 'o' && expression[i + 1] === 'r') {
-					words = [words, 'or'];
-					inSub = true;
-					i++;
-					continue;
-				}
+                if (character === 'o' && expression[i + 1] === 'r') {
+                    words = [words, 'or'];
+                    inSub = true;
+                    i++;
+                    continue;
+                }
 
-                if(character === 'a' && expression[i + 1] === 'n' && expression[i + 2] === 'd') {
+                if (character === 'a' && expression[i + 1] === 'n' && expression[i + 2] === 'd') {
                     words = [words, 'and'];
                     inSub = true;
                     i += 2;
                     continue;
                 }
-			}
+            }
 
-			if(character === '"') {
-				inQuotes = !inQuotes;
-				continue;
-			}
+            if (character === '"') {
+                inQuotes = !inQuotes;
+                continue;
+            }
 
-			word = word + character;
-		}
+            word = word + character;
+        }
 
-		pushWord(words, word, inSub);
+        pushWord(words, word, inSub);
 
-		return words;
-	}
+        return words;
+    }
 };
